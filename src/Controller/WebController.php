75f1dc4e0982c385894845506289326dcfb392e3
Azza1997 <jusuev.azret@mail.ru>
2019-07-04 16:22:00 +0600

    create class User

---
+++ src/Controller/WebController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class WebController extends AbstractController
+{
+    /**
+     * @Route("/web", name="web")
+     */
+    public function index()
+    {
+        return $this->render('web/index.html.twig', [
+            'controller_name' => 'WebController',
+        ]);
+    }
+
+    /**
+     * @Route("/web/peer_video", name="peer")
+     */
+    public function video()
+    {
+        return $this->render('web/video.html.twig', [
+            'controller_name' => 'WebController',
+        ]);
+    }
+}
