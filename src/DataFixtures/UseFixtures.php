d3f387b0d39b1464eb145a4bbf6bf199218d8dbd
Azza1997 <jusuev.azret@mail.ru>
2019-07-05 11:51:13 +0600

    Add login page and 2 users with roles

--- src/DataFixtures/UseFixtures.php
+++ src/DataFixtures/UseFixtures.php
@@ -7,9 +7,8 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
-class UserFixtures extends Fixture
+class UseFixtures extends Fixture
 {
-
     private $passwordEncoder;
 
     public function __construct(UserPasswordEncoderInterface $passwordEncoder)
@@ -19,14 +18,12 @@ class UserFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $user = new User();
-        $user->setEmail('admin@mail.com');
-        $user->setRoles(['ROLE_ADMIN']);
+        $user->setEmail('azret@mail.com');
+        $user->setRoles(['ROLE_USER']);
         $user->setPassword($this->passwordEncoder->encodePassword($user, '123123')
         );
         $manager->persist($user);
 
         $manager->flush();
     }
-
-
 }
