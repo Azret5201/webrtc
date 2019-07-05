b50d2c520b70f4f3fdba2034573c71e1ab0265dd
Azza1997 <jusuev.azret@mail.ru>
2019-07-05 16:49:08 +0600

    asd

---
+++ src/Admin/RawAdmin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Azret
+ * Date: 05.07.2019
+ * Time: 14:05
+ */
+
+namespace App\Admin;
+
+
+class RawAdmin
+{
+
+}
---
+++ src/Admin/UserAdmin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Azret
+ * Date: 05.07.2019
+ * Time: 12:00
+ */
+
+namespace App\Admin;
+
+
+class UserAdmin
+{
+
+}
